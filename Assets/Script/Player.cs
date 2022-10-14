6b29553346150b4511d2f324570c44f469d8238c
BaeGWoo <gwbae4211@naver.com>
2022-10-14 19:47:57 +0900

    Jump System

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rigid2D;
+
     [SerializeField] SpriteRenderer sprite;
     [SerializeField] float speed = 1.0f;
+    [SerializeField] float jumpPower = 1.0f;
 
     //private Vector2 direction;
 
+    private void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
 
-    
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+       
 
         if(x>0)
         {
@@ -26,14 +32,33 @@ public class Player : MonoBehaviour
             sprite.flipX = true;
         }
 
-        transform.Translate(x * speed * Time.deltaTime,y*speed*Time.deltaTime,transform.position.z);
+        transform.Translate
+            (
+                x * speed * Time.deltaTime,
+                0,
+                transform.position.z
+            );
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //ForceMode2D.Impulse : 무게를 적용할 때 사용합니다.
+            rigid2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
     }
 
 
+
+   
     //유니티 2D에서는 2D Collider랑 Rigidbody 2D는 2D 충돌 함수를 사용해야 합니다.
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("2D 충돌");
+        //Debug.Log("2D 충돌");
+        if(collision.CompareTag("Portal"))
+        {
+            Camera.main.transform.position= new Vector3(20, 0, Camera.main.transform.position.z);
+            transform.position = new Vector3(12.5f, 0, 0);
+
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
