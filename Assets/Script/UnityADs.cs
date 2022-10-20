6564a479efc0a9d14553c00abef91d8bda1196ae
BaeGWoo <gwbae4211@naver.com>
2022-10-20 19:46:57 +0900

    Ad System

--- Assets/Script/UnityADs.cs
+++ Assets/Script/UnityADs.cs
@@ -1,18 +1,92 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
 
 public class UnityADs : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private string androidID = "4979865";
+    private int gameMoney = 0;
+
     void Start()
     {
-        
+        Advertisement.Initialize(androidID);
+        ShowBanner();
     }
 
-    // Update is called once per frame
-    void Update()
+  
+    public void ShowInitialize()
     {
+        if(Advertisement.IsReady())
+        {
+            Advertisement.Show("Interstitial_Android");
+        }
+    }
+
+   public void ShowBanner()
+    {
+        if(Advertisement.IsReady("Banner_Android"))
+        {
+
+            //배너 광고의 위치설정
+            Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+
+
+            //배너 광고 보여주기
+            Advertisement.Banner.Show("Banner_Android");
+
+            
+        }
+
+        else
+        {
+            StartCoroutine(RepeateBanner());
+        }
+
+
+    }
+
+    private IEnumerator RepeateBanner()
+    {
+        yield return new WaitForSeconds(1f);
+        ShowBanner();
+    }
+
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch(result)
+        {
+            //광고를 다 봤을 때
+            case ShowResult.Finished:
+                gameMoney += 100;
+
+                break;
+
+            //광고를 스킵했을때
+            case ShowResult.Skipped:
+                Debug.Log("광고를 스킵했습니다.");
+                break;
+
+
+            //보상형 나오는 데 실패했을 때
+            case ShowResult.Failed:
+                Debug.LogError("광고 송출을 실패했습니다.");
+                break;
+
+
+        }
+    }
+
+    public void ShowRewarded()
+    {
+        if(Advertisement.IsReady())
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+
+            Advertisement.Show("Rewarded_Android", options);
+        }
+
         
     }
 }
